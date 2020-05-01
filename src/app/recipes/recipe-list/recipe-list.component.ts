fb8de34af329c5acd85450eea0e16d5ee23e6bdc
Fernando <fgironpe@gmail.com>
2020-05-01 15:15:29 +0200

    First Commit => Sección Proyect: Basics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'test', 'https://c1.wallpaperflare.com/preview/992/474/505/food-meat-recipe-power.jpg'),
+    new Recipe('A test recipe', 'test', 'https://c1.wallpaperflare.com/preview/992/474/505/food-meat-recipe-power.jpg'),
+    new Recipe('A test recipe', 'test', 'https://c1.wallpaperflare.com/preview/992/474/505/food-meat-recipe-power.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
